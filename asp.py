3f36533cf63387e364b4af0b9aa2ff86dd380767
parathwaaj k <kparathwaaj124@gmail.com>
2021-09-09 09:53:06 +0700

    task(6/9/21)

---
+++ asp.py
@@ -0,0 +1,11 @@
+
+s=("Hindustan Aeronautics Limited\tis headquartered in Bangalore")
+s2=("Hindustan Aeronautics\\Limited is headquartered in Bangalore")
+s3=("Hindustan\nAeronautics limited is headquartered in Bangalore")
+s4=("Hindustan Aeronautics\'Limited is headquartered in Bangalore")
+print(s)
+print(s2)
+print(s3)
+print(s4)
+
+
---
+++ jsonescape.py
@@ -0,0 +1,13 @@
+
+f={"\colour":"black",
+   "\category":True,
+   "type" :"pri""mary",
+   "\hex":1000}
+print(f)
+
+para={"services":"paytm",
+      "car name":"benz",
+      "item number":"its dept\\upon their car\tmodels",
+      "id":"0.34"
+      }
+print(para)
